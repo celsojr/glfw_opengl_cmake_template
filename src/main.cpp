7fe3c533dc7dba41bd4161d9ad91a8df093d6a23
Celso Jr <celsojrfull@gmail.com>
2024-10-20 14:55:47 +0400

    texture atlas preview

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,55 @@
+#include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <array>
-#include <iostream>
-
-unsigned int shaderProgram{};
-glm::mat4 rotationMatrix = glm::mat4(1.0f);
-
-constexpr auto vertexShaderSource = R"(
-    #version 330 core
-    
-    layout (location = 0) in vec3 aPos;
-    uniform mat4 rotationMatrix;
-
-    void main()
-    {
-        gl_Position = rotationMatrix * vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    }
-)";
-
-constexpr auto fragmentShaderSource = R"(
-    #version 330 core
-
-    out vec4 FragColor;
-
-    void main()
-    {
-        FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
-    }
-)";
-
-// vertices duplicated for GL_LINES
-constexpr auto squareVertices = std::array{
-    -0.5f, 0.5f, 0.0f,  // Top-left
-    0.5f, 0.5f, 0.0f,   // Top-right
-    0.5f, 0.5f, 0.0f,   // Top-right
-    0.5f, -0.5f, 0.0f,  // Bottom-right
-    0.5f, -0.5f, 0.0f,  // Bottom-right
-    -0.5f, -0.5f, 0.0f, // Bottom-left
-    -0.5f, -0.5f, 0.0f, // Bottom-left
-    -0.5f, 0.5f, 0.0f   // Top-left
-};
+// Include stb_image for loading images
+// https://github.com/nothings/stb/blob/master/stb_image.h
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 void framebufferSizeChanged(GLFWwindow *window, int width, int height);
-void processInput(GLFWwindow *window, float deltaTime);
 void render(GLFWwindow *window);
+GLuint loadTexture(const char *filepath);
+
+// Vertex and fragment shader source codes
+const char *vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec2 aTexCoord;
+
+    out vec2 TexCoord;
+
+    void main()
+    {
+        gl_Position = vec4(aPos, 1.0);
+        TexCoord = aTexCoord;
+    }
+)";
+
+const char *fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    in vec2 TexCoord;
+
+    uniform sampler2D ourTexture;
+
+    void main()
+    {
+        FragColor = texture(ourTexture, TexCoord);
+    }
+)";
+
+GLuint VAO, VBO, EBO, textureID;
+GLuint shaderProgram;
+
+const int W_WIDTH = 400, W_HEIGHT = 600;
 
 int main()
 {
     if (!glfwInit())
     {
         std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
+        return EXIT_FAILURE;
     }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -66,16 +60,13 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    const GLFWvidmode *VideoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-    const auto ScreenHeight = VideoMode->height;
+    GLFWwindow *window = glfwCreateWindow(W_WIDTH, W_HEIGHT, "Minesweeper with OpenGL", NULL, NULL);
 
-    GLFWwindow *window = glfwCreateWindow(ScreenHeight / 2, ScreenHeight / 2,
-                                          "OpenGL + GLFW", NULL, NULL);
     if (window == NULL)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return -1;
+        return EXIT_FAILURE;
     }
 
     glfwMakeContextCurrent(window);
@@ -84,112 +75,120 @@ int main()
     {
         std::cerr << "Failed to initialize GLEW" << std::endl;
         glfwTerminate();
-        return -1;
+        return EXIT_FAILURE;
     }
 
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeChanged);
+
+    // Compile shaders and link them into a program
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cout << "Vertex Shader Compilation Failed:" << infoLog << std::endl;
-    }
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cout << "Fragment Shader Compilation Failed:" << infoLog << std::endl;
-    }
 
     shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        std::cout << "Shader Program Linking Failed: %s" << infoLog << std::endl;
-    }
-
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    unsigned int VBO, VAO;
+    // Set stb_image to flip loaded textures on the y-axis
+    stbi_set_flip_vertically_on_load(true);
+
+    // Define vertices and texture coordinates for a quad (will be scaled to texture size)
+    // float aspectRatio = 384.0f / 32.0f;
+    float scaleX = 384.0f / (float)W_WIDTH; // Width of texture relative to window width
+    float scaleY = 32.0f / (float)W_HEIGHT; // Height of texture relative to window height
+
+    float vertices[] = {
+        // scale          // texture coords
+        -scaleX, -scaleY, 0.0f, 0.0f, 0.0f, // bottom left
+        scaleX, -scaleY, 0.0f, 1.0f, 0.0f,  // bottom right
+        scaleX, scaleY, 0.0f, 1.0f, 1.0f,   // top right
+        -scaleX, scaleY, 0.0f, 0.0f, 1.0f   // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0};
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER,
-                 squareVertices.size() * sizeof(float),
-                 squareVertices.data(),
-                 GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
-    glfwSetFramebufferSizeCallback(window, framebufferSizeChanged);
+    // Texture coordinate attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-    float lastFrameStartTime = 0.0f;
+    textureID = loadTexture("res/texture/atlas.png");
+
+    if (textureID == 0)
+    {
+        std::cerr << "Failed to load texture" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
 
     while (!glfwWindowShouldClose(window))
     {
-        float currentFrameStartTime = static_cast<float>(glfwGetTime());
-        float deltaTime = currentFrameStartTime - lastFrameStartTime;
-        lastFrameStartTime = currentFrameStartTime;
-
-        processInput(window, deltaTime);
         render(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
-
     glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 void framebufferSizeChanged(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
-    render(window);
 }
 
-void processInput(GLFWwindow *window, float deltaTime)
+GLuint loadTexture(const char *filepath)
 {
-    const float rotationSpeedDegreesPerSecond = 90.0f;
-    const float rotationAnglePerFrame = rotationSpeedDegreesPerSecond * deltaTime;
+    GLuint textureID;
+    glGenTextures(1, &textureID);
 
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(filepath, &width, &height, &nrChannels, 0);
+    if (data)
     {
-        rotationMatrix = glm::rotate(rotationMatrix,
-                                     glm::radians(rotationAnglePerFrame),
-                                     glm::vec3(0.0f, 0.0f, 1.0f));
+        GLenum format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
     }
-    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+    else
     {
-        rotationMatrix = glm::rotate(rotationMatrix,
-                                     glm::radians(-rotationAnglePerFrame),
-                                     glm::vec3(0.0f, 0.0f, 1.0f));
+        std::cerr << "Failed to load texture from " << filepath << std::endl;
+        return EXIT_FAILURE;
     }
+
+    return textureID;
 }
 
 void render(GLFWwindow *window)
@@ -197,12 +196,12 @@ void render(GLFWwindow *window)
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // Bind texture and shader program
     glUseProgram(shaderProgram);
+    glBindTexture(GL_TEXTURE_2D, textureID);
 
-    auto location = glGetUniformLocation(shaderProgram, "rotationMatrix");
-    glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(rotationMatrix));
-
-    glDrawArrays(GL_LINES, 0, static_cast<GLsizei>(squareVertices.size() / 3));
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
 }
